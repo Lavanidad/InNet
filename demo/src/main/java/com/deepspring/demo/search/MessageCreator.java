52938498feff5b178f18aed51cc442e672366c57
lavanidad <754458151@qq.com>
2019-07-05 11:52:54 +0800

    broadcast updata

---
+++ demo/src/main/java/com/deepspring/demo/search/MessageCreator.java
@@ -0,0 +1,31 @@
+package com.deepspring.demo.search;
+
+/**
+ * Created by fzy on 2019/7/5.
+ */
+public class MessageCreator {
+    private static final String SN_HEADER = "sn收到口令:";
+    public static final String PORT_HEADER = "口令,回复端口:";
+
+    public static String buildWithPort(int port) {
+        return PORT_HEADER + port;
+    }
+
+    public static int parsePort(String data) {
+        if (data.startsWith(PORT_HEADER)) {
+            return Integer.parseInt(data.substring(PORT_HEADER.length()));
+        }
+        return -1;
+    }
+
+    public static String buildWithSn(String sn) {
+        return SN_HEADER +sn;
+    }
+
+    public static String parseSn(String data) {
+        if (data.startsWith(SN_HEADER)) {
+            return data.substring(SN_HEADER.length());
+        }
+        return null;
+    }
+}
--- demo/src/main/java/com/deepspring/demo/search/UDPProvider.java
+++ demo/src/main/java/com/deepspring/demo/search/UDPProvider.java
@@ -3,7 +3,7 @@ package com.deepspring.demo.search;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.util.UUID;
 
 /**
  * UDP提供者，提供服务
@@ -11,34 +11,80 @@ import java.net.SocketException;
  */
 public class UDPProvider {
 
+
     public static void main(String[] args) throws IOException {
-        System.out.println("UDPProvider start");
-        //作为接受者，指定一个端口进行接收
-        DatagramSocket ds = new DatagramSocket(20000);
+        //生成一份唯一标识
+        String sn = UUID.randomUUID().toString();
+        Provider provider = new Provider(sn);
+        provider.start();
+        //读取任一键盘信息后可以退出
+        System.in.read();
+        provider.exit();
+    }
 
-        //构建接收实体
-        final byte[] buf = new byte[512];
-        DatagramPacket receivePack = new DatagramPacket(buf, buf.length);
+    private static class Provider extends Thread {
 
-        //接收
-        ds.receive(receivePack);
-        //打印接收到的信息与发送者的信息
-        String ip = receivePack.getAddress().getHostAddress();
-        int port = receivePack.getPort();
-        int dataLen = receivePack.getLength();
-        String data = new String(receivePack.getData(), 0, dataLen);
-        System.out.println("UDPProvider receive from ip:" + ip + "\tport:" + port
-                + "\tdata:" + data);
+        private final String sn;
+        private boolean done = false;
+        private DatagramSocket ds = null;
 
-        //构建回送数据
-        String responseData = "Receive data with len:" + dataLen;
-        byte[] responseDataBytes = responseData.getBytes();
-        //直接根据发送者构建一份回送信息
-        DatagramPacket responsePacket = new DatagramPacket(responseDataBytes, responseDataBytes.length,
-                receivePack.getAddress(), receivePack.getPort());
-        ds.send(responsePacket);
-        //完成
-        System.out.println("UDPProvider Finished");
-        ds.close();
+        public Provider(String sn) {
+            super();
+            this.sn = sn;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            System.out.println("UDPProvider start");
+            try {
+                //构建监听
+                ds = new DatagramSocket(20000);
+                while (!done) {
+                    //构建接收实体
+                    final byte[] buf = new byte[512];
+                    DatagramPacket receivePack = new DatagramPacket(buf, buf.length);
+
+                    //接收
+                    ds.receive(receivePack);
+                    //打印接收到的信息与发送者的信息
+                    String ip = receivePack.getAddress().getHostAddress();
+                    int port = receivePack.getPort();
+                    int dataLen = receivePack.getLength();
+                    String data = new String(receivePack.getData(), 0, dataLen);
+                    System.out.println("UDPProvider receive from ip:" + ip + "\tport:" + port
+                            + "\tdata:" + data);
+
+                    //解析端口号
+                    int responsePort = MessageCreator.parsePort(data);
+                    if (responsePort != -1) {
+                        //构建回送数据
+                        String responseData = MessageCreator.buildWithSn(sn);
+                        byte[] responseDataBytes = responseData.getBytes();
+                        //直接根据发送者构建一份回送信息
+                        DatagramPacket responsePacket = new DatagramPacket(responseDataBytes, responseDataBytes.length,
+                                receivePack.getAddress(), receivePack.getPort());
+                        ds.send(responsePacket);
+                    }
+                }
+            } catch (Exception ignored) {
+            } finally {
+                close();
+            }
+            //完成
+            System.out.println("UDPProvider Finished");
+        }
+
+        private void close() {
+            if (ds != null) {
+                ds.close();
+                ds = null;
+            }
+        }
+
+        void exit() {
+            done = true;
+            close();
+        }
     }
 }
